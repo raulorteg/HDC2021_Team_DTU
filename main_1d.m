2d39fcc05d78360e73086343f9f3a0f3277822f0
madsemildahlgaard <madsemildh@gmail.com>
2021-09-20 10:41:55 +0200

    Main 1d function

--- main_1d.m
+++ main_1d.m
@@ -4,35 +4,29 @@ close all
 
 % Set parameters.
 true_radius = 4;
-true_noise_std = 0.015;
-mu_r = 3.9;
-delta_r = 0.5;
+mu_r = 2;
+delta_r = 0.1;
 Sr = 100;
 sigma_e = 0.015;
 lambda_tv = 0.01;
-y = 30;
+y = 800;
 
-use_chol = 0;
+use_chol = 1;
 
 if use_chol == 0
-    lambda_tv = lambda_tv*true_noise_std^2;
+    lambda_tv = lambda_tv*sigma_e^2;
 end
 
-K = 100;
+K = 40;
 
 % Read in test image.
-im = im2double(imread('data/test.jpg'));
-im = imresize(im, 1); % DOWNSCALE IMAGE BEFORE USE FOR FASTER COMPUTATION IN TESTS
+im = rgb2gray(im2double(imread('data/competition_example.png')));
 
-% Blur and add noise.
-im_blurred =  convb(im, true_radius);
-im_blurred_noise = im_blurred + randn(size(im_blurred))*true_noise_std;
-b = rescale(im_blurred_noise); % scale between 0 and 1.
-b = b(y,:);
-
+% b = rescale(im_blurred_noise); % scale between 0 and 1.
+b = im(y,:);
 
 % Initial guess
-x = ones(size(b));
+x = zeros(size(b));
 
 disp(['it: ', num2str(0)])
 disp(['  mu_r: ', num2str(mu_r)])
@@ -41,10 +35,13 @@ disp(['  delta_r: ', num2str(delta_r)])
 for k = 1:K
     
     % Update x
-    [x,f_vec] = x_update(x, mu_r, delta_r, b, sigma_e, Sr, lambda_tv, use_chol);
+    x = x_update_1d(x, mu_r, delta_r, b, sigma_e, Sr, lambda_tv, use_chol);
+    
+    x = medfilt1(x);
+%     x(x<0.1) = 0;
     
     % Update r
-    [mu_r, delta_r] = r_update(x, b, mu_r, delta_r, sigma_e, Sr);
+    [mu_r, delta_r] = r_update_1d(x, b, mu_r, delta_r, sigma_e, Sr);
     
     if mod(k, 10)==0
         disp(['it: ', num2str(k)])
@@ -53,8 +50,10 @@ for k = 1:K
     end
 end
 
-[final, f_vec] = x_update(zeros(size(im_blurred_noise)), mu_r, delta_r, im_blurred_noise, sigma_e, Sr, lambda_tv, use_chol);
-
+x0 = zeros(size(im));
+lambda_final = 0.1;
+final = FISTA_TVsmooth(mu_r, im, lambda_final, x0);
+%%
 % Show figures.
 figure(1); 
 imagesc(im); title('Original'); 
@@ -62,14 +61,7 @@ h = colorbar;
 h.Limits = [0 1];
 colormap('gray');
 
-figure(2); 
-imagesc(im_blurred_noise); 
-title('Blurred with noise'); 
-h = colorbar; 
-h.Limits = [0 1];
-colormap('gray');
-
-figure(3);
+figure(2);
 plot(x, 'b');
 hold on
 plot(im(y,:), 'r')
@@ -77,7 +69,7 @@ legend('Estimated', 'True')
 title(['\lambda = ' num2str(lambda_tv)]); 
 colormap('gray');
 
-figure(4);
+figure(3);
 imagesc(final); 
 title(['\lambda = ' num2str(lambda_tv)]); 
 h = colorbar; 
