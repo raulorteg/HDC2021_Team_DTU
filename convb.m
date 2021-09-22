294452ee958717be54dced75889da7b34a2cf326
madsemildahlgaard <madsemildh@gmail.com>
2021-09-22 21:52:08 +0200

    Change convb to work with 1d as well

--- convb.m
+++ convb.m
@@ -3,7 +3,18 @@ function c=convb(g,r)
 %function c=convb_outfocus(g,r);
 %r is the radius of the point spread function.
 
-PSF = fspecial('disk', r);
-p = (size(PSF, 1) - 1) / 2;
-g = padarray(g,[p p], 'symmetric');
-c = conv2(g, PSF, 'valid');
+if isvector(g)
+    g = g(:);
+    PSF = fspecial('disk', r);
+    p = (size(PSF, 1) - 1) / 2;
+    PSF = PSF(p+1,:);
+    PSF = PSF/sum(PSF);
+    g2 = padarray(g, [p,0], 'symmetric');
+    c = conv(g2, PSF, 'valid');
+else
+    PSF = fspecial('disk', r);
+    p = (size(PSF, 1) - 1) / 2;
+    g = padarray(g,[p p], 'symmetric');
+    c = conv2(g, PSF, 'valid');
+end
+
--- main_1d.m
+++ main_1d.m
@@ -3,24 +3,25 @@ clc
 close all
 
 % Set parameters.
-true_radius = 4;
-mu_r = 2;
-delta_r = 0.1;
+mu_r = 40;
+delta_r = 2;
 Sr = 100;
-sigma_e = 0.015;
-lambda_tv = 0.01;
+lambda_tv = 0.001;
 y = 800;
+K = 100;
+
+% Read in test image.
+im = im2double(imread('data/step5.png'));
+% im = rescale(im);
+
+noise = im(100:200, 100:200);
+sigma_e = std(noise(:));
 
 use_chol = 1;
 
 if use_chol == 0
     lambda_tv = lambda_tv*sigma_e^2;
 end
-
-K = 40;
-
-% Read in test image.
-im = rgb2gray(im2double(imread('data/competition_example.png')));
 
 % b = rescale(im_blurred_noise); % scale between 0 and 1.
 b = im(y,:);
@@ -35,28 +36,30 @@ disp(['  delta_r: ', num2str(delta_r)])
 for k = 1:K
     
     % Update x
-    x = x_update_1d(x, mu_r, delta_r, b, sigma_e, Sr, lambda_tv, use_chol);
+    x = x_update(x, mu_r, delta_r, b, sigma_e, Sr, lambda_tv, use_chol);
     
     x = medfilt1(x);
-%     x(x<0.1) = 0;
+%     x(x>0.9) = 1;
     
     % Update r
     [mu_r, delta_r] = r_update_1d(x, b, mu_r, delta_r, sigma_e, Sr);
     
-    if mod(k, 10)==0
+    if mod(k, 5)==0
         disp(['it: ', num2str(k)])
         disp(['  mu_r: ', num2str(mu_r)])
         disp(['  delta_r: ', num2str(delta_r)])
     end
 end
 
+
 x0 = zeros(size(im));
-lambda_final = 0.1;
+lambda_final = 0.01;
 final = FISTA_TVsmooth(mu_r, im, lambda_final, x0);
-%%
+
 % Show figures.
 figure(1); 
-imagesc(im); title('Original'); 
+imagesc(im); 
+title('Original'); 
 h = colorbar; 
 h.Limits = [0 1];
 colormap('gray');
@@ -71,7 +74,7 @@ colormap('gray');
 
 figure(3);
 imagesc(final); 
-title(['\lambda = ' num2str(lambda_tv)]); 
+title('Deblurred'); 
 h = colorbar; 
 h.Limits = [0 1];
 colormap('gray');
